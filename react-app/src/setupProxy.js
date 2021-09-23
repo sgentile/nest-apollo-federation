7129fc7f29abb7408d3e6eee58256a2b9cc71154
Steve Gentile <steven.gentile@gmail.com>
2021-09-23 07:47:35 -0400

    clean up proxy file

--- react-app/src/setupProxy.js
+++ react-app/src/setupProxy.js
@@ -1,8 +1,6 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 require("dotenv").config();
 module.exports = function (app) {
-  // console.log('setupProxy', process.env.REACT_APP_TECHSCOUT_AP);
-
   app.use(
     "/api",
     createProxyMiddleware({
@@ -12,18 +10,6 @@ module.exports = function (app) {
       //   autoRewrite: true,
     })
   );
-
-  //   app.use(
-  //     process.env.REACT_APP_SUBSCRIPTIONS_API_URL,
-  //     createProxyMiddleware({
-  //       target: process.env.REACT_APP_SUBSCRIPTIONS_API_URL,
-  //       // pathRewrite: { "^/socket": "" },
-  //       ws: true,
-  //       logLevel: "debug",
-  //       changeOrigin: true,
-  //       //   autoRewrite: true,
-  //     })
-  //   );
   app.use(
     "/graphql",
     createProxyMiddleware({
@@ -31,16 +17,4 @@ module.exports = function (app) {
       ws: true,
     })
   );
-
-  //   app.use(
-  //     "/ws",
-  //     createProxyMiddleware({
-  //       target: process.env.REACT_APP_SUBSCRIPTIONS_API_URL,
-  //       // pathRewrite: { "^/ws": "" },
-  //       ws: true,
-  //       changeOrigin: true,
-  //       // autoRewrite: true,
-  //       logLevel: "debug",
-  //     })
-  //   );
 };
