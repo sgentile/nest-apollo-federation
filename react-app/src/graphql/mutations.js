a3a1875cc37053420064cce87b9d88a159f98039
Steve Gentile <steven.gentile@gmail.com>
2021-09-24 13:04:57 -0400

    fix createPost query

--- react-app/src/graphql/mutations.js
+++ react-app/src/graphql/mutations.js
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const AddPost = gql`
   mutation generatePost($title: String!, $content: String!, $userId: Int!) {
-    createPost(input: { title: $title, content: $content, userId: 1 }) {
+    createPost(input: { title: $title, content: $content, userId: $userId }) {
       id
       title
       content
